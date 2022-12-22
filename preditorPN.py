0a16aad4dc1001830afac63464adbda09bf4a35a
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-12-22 17:19:29 -0300

    Update preditorPN.py

--- preditorPN.py
+++ preditorPN.py
@@ -8,14 +8,14 @@ APP positivos negativos COVID-19
 import streamlit as st
 import joblib
 
-classes = ['COVID-19 negativo','COVID-19 positivo']
-st.title('Preditor para COVID-19')
+classes = ['COVID-19 negative','COVID-19 positive']
+st.title('Predict for COVID-19')
 modelo = joblib.load('giane-pn.sav')
-mono = st.number_input('Monócitos (/mm3)')
-linf = st.number_input('Linfócitos (/mm3)')
-plaq = st.number_input('Plaquetas (/mm3)')
+mono = st.number_input('Monocytes (/mm3)')
+linf = st.number_input('Lymphocytes (/mm3)')
+plaq = st.number_input('Platelets (/mm3)')
 pcr = st.number_input('PCR (mg/dL)')
-ferr = st.number_input('Ferritina (ng/mL)')
+ferr = st.number_input('ferritin (ng/mL)')
 pac = [mono,linf,plaq,pcr,ferr]
 
 pred = modelo.predict([pac])
